9676c21c0a5343e2bef780c5575e396f5c16ac10
BulbaSprout <pasabozenko3@gmail.com>
2025-10-25 18:42:10 +0300

    make upgrades work and make gaame somewhat playable

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var ctx = canvas.getContext("2d");
 var progresses = [0, 0, 0]
 var progressSpeeds = [0.01, 0.002, 0.001]
 var unlocks = [true, false, false]
+var upgradeCosts = [75, 250, 5000]
+var spinMoney = [10, 100, 500]
+var upgradePerks = [0.01, 0.003, 0.001]
+var priceMult = [1.05, 1.015, 1.004]
+var money = 0
 
 requestAnimationFrame(render)
 
@@ -10,14 +15,13 @@ requestAnimationFrame(render)
 function render() {
     height = window.innerHeight
     width = window.innerWidth
-    canvas.height = height / 100 * 99.5
-    canvas.width = width / 100 * 99.9
+    canvas.height = height
+    canvas.width = width
 
     //circles themselves
     ctx.lineWidth = width / 103
     if (unlocks[2]) {
         ctx.beginPath()
-        ctx.moveTo(getCenter(0, "x"), getCenter(height / 30, "y"));
         ctx.arc(getCenter(0, "x"), getCenter(height / 30, "y"), width / 14, -Math.PI / 2, (-Math.PI / 2) + progresses[2] * Math.PI * 2)
         ctx.strokeStyle = '#f0ea24ff'
         ctx.stroke()
@@ -25,21 +29,19 @@ function render() {
 
     if (unlocks[1]) {
         ctx.beginPath()
-        ctx.moveTo(getCenter(0, "x"), getCenter(height / 30, "y"));
         ctx.arc(getCenter(0, "x"), getCenter(height / 30, "y"), width / 17, -Math.PI / 2, (-Math.PI / 2) + progresses[1] * Math.PI * 2)
         ctx.strokeStyle = '#e7922aff'
         ctx.stroke()
     }
 
     ctx.beginPath()
-    ctx.moveTo(getCenter(0, "x"), getCenter(height / 30, "y"));
+    //ctx.moveTo(getCenter(0, "x"), getCenter(height / 30, "y"));
     ctx.arc(getCenter(0, "x"), getCenter(height / 30, "y"), width / 22, -Math.PI / 2, (-Math.PI / 2) + progresses[0] * Math.PI * 2)
     ctx.strokeStyle = 'rgba(222, 34, 34, 1)'
     ctx.stroke()
 
 
 
-    midButtons()
     progress()
     requestAnimationFrame(render) // keep on bottom!
 }
@@ -49,38 +51,37 @@ function getCenter(size, axis) {
     else if (axis == "y") return height / 2 - size / 2
 }
 
-function midButtons() {
-    //outter button
-    ctx.beginPath()
-    ctx.arc(getCenter(0, "x"), getCenter(height / 30, "y"), width / 25, 0, Math.PI * 2)
-    ctx.fillStyle = '#555555ff'
-    ctx.strokeStyle = '#3a3a3aff'
-    ctx.lineWidth = 2
-    ctx.fill()
-    ctx.stroke()
 
-    //inner button
-    ctx.beginPath()
-    ctx.arc(getCenter(0, "x"), getCenter(height / 30, "y"), width / 32, 0, Math.PI * 2)
-    ctx.fillStyle = '#d72424ff'
-    ctx.strokeStyle = '#821d1dff'
-    ctx.lineWidth = 2
-    ctx.fill()
-    ctx.stroke()
-
-}
 
 function progress() {
     var index = 0
     progresses.forEach(function () {
-        progresses[index] += progressSpeeds[index]
+        if (progressSpeeds[index] >= 1){
+            progresses[index] = 1
+        }
+
+        if (unlocks[index]) progresses[index] += progressSpeeds[index]
         if (progresses[index] >= 1) {
-            progresses[index] = 0
+            if (progressSpeeds[index] < 1){
+            progresses[index] = 0}
+            money += spinMoney[index]
+            document.getElementById("moneyDisplay").innerText = "Money: "+money
         }
         index++
     })
 }
 
+function upgrade(id) {
+    if (unlocks[id]) {
+        if (money > upgradeCosts[id]) {
+            money -= upgradeCosts[id]
+            progressSpeeds[id] += upgradePerks[id]
+            unlocks[id + 1] = true
+            upgradeCosts[id] = Math.round(upgradeCosts[id] * priceMult[id])
+            document.getElementById("upg"+(id+1)).innerText = "1. +"+upgradePerks[id]+"0.01 Speed\nPrice: "+upgradeCosts[id]+"$"
+        }
+    }
+}
 document.querySelector('.upgrade-tab').addEventListener('click', function (e) {
     if (e.target.tagName.toLowerCase() === 'button') return;
     this.classList.toggle('active');
