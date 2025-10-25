6d57875e0b64fc34c77312c6fb18bb11f5987992
BulbaSprout <pasabozenko3@gmail.com>
2025-10-25 17:46:24 +0300

    add upgrades menu that doesnt do anything

--- script.js
+++ script.js
@@ -7,31 +7,33 @@ var unlocks = [true, false, false]
 requestAnimationFrame(render)
 
 
-function render(){
+function render() {
     height = window.innerHeight
     width = window.innerWidth
-    canvas.height = height/100*99.5
-    canvas.width = width/100*99.9
+    canvas.height = height / 100 * 99.5
+    canvas.width = width / 100 * 99.9
 
     //circles themselves
-    ctx.lineWidth = width/103
-    if (unlocks[2]){
-    ctx.beginPath()
-    ctx.moveTo(getCenter(0, "x"), getCenter(height/30, "y"));
-    ctx.arc(getCenter(0, "x"), getCenter(height/30, "y"), width/14, -Math.PI/2, (-Math.PI/2) + progresses[2] * Math.PI*2)
-    ctx.strokeStyle = '#f0ea24ff'
-    ctx.stroke()}
+    ctx.lineWidth = width / 103
+    if (unlocks[2]) {
+        ctx.beginPath()
+        ctx.moveTo(getCenter(0, "x"), getCenter(height / 30, "y"));
+        ctx.arc(getCenter(0, "x"), getCenter(height / 30, "y"), width / 14, -Math.PI / 2, (-Math.PI / 2) + progresses[2] * Math.PI * 2)
+        ctx.strokeStyle = '#f0ea24ff'
+        ctx.stroke()
+    }
 
-    if (unlocks[1]){
-    ctx.beginPath()
-    ctx.moveTo(getCenter(0, "x"), getCenter(height/30, "y"));
-    ctx.arc(getCenter(0, "x"), getCenter(height/30, "y"), width/17, -Math.PI/2, (-Math.PI/2) + progresses[1] * Math.PI*2)
-    ctx.strokeStyle = '#e7922aff'
-    ctx.stroke()}
+    if (unlocks[1]) {
+        ctx.beginPath()
+        ctx.moveTo(getCenter(0, "x"), getCenter(height / 30, "y"));
+        ctx.arc(getCenter(0, "x"), getCenter(height / 30, "y"), width / 17, -Math.PI / 2, (-Math.PI / 2) + progresses[1] * Math.PI * 2)
+        ctx.strokeStyle = '#e7922aff'
+        ctx.stroke()
+    }
 
     ctx.beginPath()
-    ctx.moveTo(getCenter(0, "x"), getCenter(height/30, "y"));
-    ctx.arc(getCenter(0, "x"), getCenter(height/30, "y"), width/22, -Math.PI/2, (-Math.PI/2) + progresses[0] * Math.PI*2)
+    ctx.moveTo(getCenter(0, "x"), getCenter(height / 30, "y"));
+    ctx.arc(getCenter(0, "x"), getCenter(height / 30, "y"), width / 22, -Math.PI / 2, (-Math.PI / 2) + progresses[0] * Math.PI * 2)
     ctx.strokeStyle = 'rgba(222, 34, 34, 1)'
     ctx.stroke()
 
@@ -42,15 +44,15 @@ function render(){
     requestAnimationFrame(render) // keep on bottom!
 }
 
-function getCenter(size, axis){
-    if (axis == "x")return width/2-size/2
-    else if (axis == "y") return height/2-size/2
+function getCenter(size, axis) {
+    if (axis == "x") return width / 2 - size / 2
+    else if (axis == "y") return height / 2 - size / 2
 }
 
-function midButtons(){
+function midButtons() {
     //outter button
     ctx.beginPath()
-    ctx.arc(getCenter(0, "x"), getCenter(height/30, "y"), width/25, 0, Math.PI*2)
+    ctx.arc(getCenter(0, "x"), getCenter(height / 30, "y"), width / 25, 0, Math.PI * 2)
     ctx.fillStyle = '#555555ff'
     ctx.strokeStyle = '#3a3a3aff'
     ctx.lineWidth = 2
@@ -59,7 +61,7 @@ function midButtons(){
 
     //inner button
     ctx.beginPath()
-    ctx.arc(getCenter(0, "x"), getCenter(height/30, "y"), width/32, 0, Math.PI*2)
+    ctx.arc(getCenter(0, "x"), getCenter(height / 30, "y"), width / 32, 0, Math.PI * 2)
     ctx.fillStyle = '#d72424ff'
     ctx.strokeStyle = '#821d1dff'
     ctx.lineWidth = 2
@@ -68,13 +70,18 @@ function midButtons(){
 
 }
 
-function progress(){
+function progress() {
     var index = 0
     progresses.forEach(function () {
         progresses[index] += progressSpeeds[index]
-        if (progresses[index] >= 1.01) {
+        if (progresses[index] >= 1) {
             progresses[index] = 0
-        } 
+        }
         index++
     })
 }
+
+document.querySelector('.upgrade-tab').addEventListener('click', function (e) {
+    if (e.target.tagName.toLowerCase() === 'button') return;
+    this.classList.toggle('active');
+});
